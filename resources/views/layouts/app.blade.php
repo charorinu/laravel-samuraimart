67260a408cc17a3bd6c8c7a2f67ef49e688201b0
charorinu <ausuta.gawa1118@gmail.com>
2024-09-16 18:58:42 +0900

    8章 Bootstrapを導入 Font Awesomeを導入再入力

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,10 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
          <!-- Bootstrap -->
          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+         <!-- Font Awesome -->
+         <script src="https://kit.fontawesome.com/f89d35f762.js" crossorigin="anonymous"></script>
+    
     </head>
      <body>
          <div id="app">
