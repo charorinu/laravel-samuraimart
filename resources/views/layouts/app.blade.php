b9deb462488d5aa2176b75b3459688efa854138c
charorinu <ausuta.gawa1118@gmail.com>
2024-09-17 17:55:46 +0900

    10章 フッターを追加  フッターをコンポーネントとして追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,14 @@
 
          @component('components.header')
          @endcomponent
-                  
-             <main class="py-4">
+
+         <main class="py-4 mb-5">
                  @yield('content')
              </main>
+
+             @component('components.footer')
+             @endcomponent
+             
          </div>
  
          <!-- Scripts -->
