3e766935caa54eb498f5c3d96cc8c1d671e39654
charorinu <ausuta.gawa1118@gmail.com>
2024-09-24 13:15:17 +0900

    14章 検索機能とカテゴリの絞り込み機能を追加 検索機能を追加 ビューを修正

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,15 +2,15 @@
      <div class="container">
          <a class="navbar-brand" href="{{ url('/') }}">
             <img src="{{asset('img/logo.jpg')}}">
-         </a>
-         <form class="row g-1">
-             <div class="col-auto">
-                 <input class="form-control samuraimart-header-search-input">
-             </div>
-             <div class="col-auto">
-                 <button type="submit" class="btn samuraimart-header-search-button"><i class="fas fa-search samuraimart-header-search-icon"></i></button>
-             </div>
-         </form>
+            </a>
+         <form action="{{ route('products.index') }}" method="GET" class="row g-1">
+            <div class="col-auto">
+                 <input class="form-control samuraimart-header-search-input" name="keyword">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn samuraimart-header-search-button"><i class="fas fa-search samuraimart-header-search-icon"></i></button>
+            </div>
+        </form>
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
              <span class="navbar-toggler-icon"></span>
          </button>
