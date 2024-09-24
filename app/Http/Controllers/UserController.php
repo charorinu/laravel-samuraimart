7097f45a2aa910a424e5a71e6b055ce52e939443
charorinu <ausuta.gawa1118@gmail.com>
2024-09-24 18:44:30 +0900

    16章 ユーザー情報を編集 コントローラーを作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,10 +21,14 @@ class UserController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
+
     public function edit(User $user)
     {
-        //
+         $user = Auth::user();
+ 
+         return view('users.edit', compact('user'));
     }
+
 
     /**
      * Update the specified resource in storage.
@@ -34,7 +38,16 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, User $user)
-    {
-        //
+    {         
+        $user = Auth::user();
+ 
+        $user->name = $request->input('name') ? $request->input('name') : $user->name;
+        $user->email = $request->input('email') ? $request->input('email') : $user->email;
+        $user->postal_code = $request->input('postal_code') ? $request->input('postal_code') : $user->postal_code;
+        $user->address = $request->input('address') ? $request->input('address') : $user->address;
+        $user->phone = $request->input('phone') ? $request->input('phone') : $user->phone;
+        $user->update();
+
+        return to_route('mypage');
     }
 }
