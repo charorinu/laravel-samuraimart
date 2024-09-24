292edb2f407df5ea994ee0b1292fd8c392fa8e3c
charorinu <ausuta.gawa1118@gmail.com>
2024-09-24 13:26:38 +0900

    14章 検索機能とカテゴリの絞り込み機能を追加 検索機能を追加 コントローラを修正

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,19 +15,25 @@ class ProductController extends Controller
      */
     public function index(Request $request)
     {
-         if ($request->category !== null) {
-             $products = Product::where('category_id', $request->category)->paginate(15);
-             $total_count = Product::where('category_id', $request->category)->count();
-             $category = Category::find($request->category);
-         } else {
-             $products = Product::paginate(15);
-             $total_count = "";
+         $keyword = $request->keyword;
+
+        if ($request->category !== null) {
+            $products = Product::where('category_id', $request->category)->paginate(15);
+            $total_count = Product::where('category_id', $request->category)->count();
+            $category = Category::find($request->category);
+         } elseif ($keyword !== null) {
+             $products = Product::where('name', 'like', "%{$keyword}%")->paginate(15);
+             $total_count = $products->total();
              $category = null;
-         }
+        } else {
+            $products = Product::paginate(15);
+            $total_count = "";
+            $category = null;
+        }
         $categories = Category::all();
         $major_category_names = Category::pluck('major_category_name')->unique();
 
-         return view('products.index', compact('products', 'category', 'categories', 'major_category_names', 'total_count'));
+        return view('products.index', compact('products', 'category', 'categories', 'major_category_names', 'total_count', 'keyword'));
     }
 
     /**
