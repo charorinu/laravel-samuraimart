6fd31329fbde73899226a78df5c2859f7e2afabf
charorinu <ausuta.gawa1118@gmail.com>
2024-10-01 15:14:12 +0900

    30章 管理画面を実装 売り上げの管理画面を追加

--- app/Admin/Controllers/ShoppingCartController.php
+++ app/Admin/Controllers/ShoppingCartController.php
@@ -25,63 +25,28 @@ class ShoppingCartController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new ShoppingCart());
+        $grid->column('identifier', __('ID'))->sortable();
+        $grid->column('instance', __('User ID'))->sortable();
+        $grid->column('price_total', __('Price total'))->totalRow();
+        $grid->column('qty', __('Qty'))->totalRow();
+        $grid->column('created_at', __('Created at'))->sortable();
+        $grid->column('updated_at', __('Updated at'))->sortable();
 
-        $grid->column('identifier', __('Identifier'));
-        $grid->column('instance', __('Instance'));
-        $grid->column('content', __('Content'));
-        $grid->column('number', __('Number'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
-        $grid->column('buy_flag', __('Buy flag'));
-        $grid->column('code', __('Code'));
-        $grid->column('price_total', __('Price total'));
-        $grid->column('qty', __('Qty'));
+        $grid->filter(function($filter) {
+            $filter->disableIdFilter();
+            $filter->equal('identifier', 'ID');
+            $filter->equal('instance', 'User ID');
+            $filter->between('created_at', '登録日')->datetime();
+        });
+
+        $grid->disableCreateButton();
+        $grid->actions(function ($actions) {
+            $actions->disableDelete();
+            $actions->disableEdit();
+            $actions->disableView();
+        });
 
         return $grid;
     }
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(ShoppingCart::findOrFail($id));
-
-        $show->field('identifier', __('Identifier'));
-        $show->field('instance', __('Instance'));
-        $show->field('content', __('Content'));
-        $show->field('number', __('Number'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
-        $show->field('buy_flag', __('Buy flag'));
-        $show->field('code', __('Code'));
-        $show->field('price_total', __('Price total'));
-        $show->field('qty', __('Qty'));
-
-        return $show;
-    }
-
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new ShoppingCart());
-
-        $form->text('identifier', __('Identifier'));
-        $form->text('instance', __('Instance'));
-        $form->textarea('content', __('Content'));
-        $form->number('number', __('Number'));
-        $form->switch('buy_flag', __('Buy flag'));
-        $form->text('code', __('Code'));
-        $form->number('price_total', __('Price total'));
-        $form->number('qty', __('Qty'));
-
-        return $form;
-    }
 }
