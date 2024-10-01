c60ee7cdf8a0a14234b6f4fa1a487ec5adb948d9
charorinu <ausuta.gawa1118@gmail.com>
2024-10-01 15:01:18 +0900

    29章 送料の自動計算機能を実装 送料を自動計算

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -14,18 +14,30 @@ class CartController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $cart = Cart::instance(Auth::user()->id)->content();
- 
-        $total = 0;
 
-        foreach ($cart as $c) {
-            $total += $c->qty * $c->price;
+public function index()
+{
+    $cart = Cart::instance(Auth::user()->id)->content();
+
+    $total = 0;
+        $has_carriage_cost = false;
+       $carriage_cost = 0;
+
+    foreach ($cart as $c) {
+        $total += $c->qty * $c->price;
+            if ($c->options->carriage) {
+                $has_carriage_cost = true;
+            }
+    }
+
+        if($has_carriage_cost) {
+            $total += env('CARRIAGE');
+            $carriage_cost = env('CARRIAGE');
         }
 
-        return view('carts.index', compact('cart', 'total'));
-    }
+        return view('carts.index', compact('cart', 'total', 'carriage_cost'));
+}
+
     
     /**
      * Store a newly created resource in storage.
